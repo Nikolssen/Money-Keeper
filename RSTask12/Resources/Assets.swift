d78e6f5e8b6b89bc01b61f7c9bc8a59c5d24f55d
Ivan Budovich <ibudovichivan3@gmail.com>
2021-09-23 11:48:27 +0300

    Add assets

---
+++ RSTask12/Resources/Assets.swift
@@ -0,0 +1,46 @@
+//
+//  Assets.swift
+//  RSTask12
+//
+//  Created by Ivan Budovich on 9/23/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let amaranthRed = UIColor(named: "Amaranth Red")!
+    static let greenBlueCrayola = UIColor(named: "Green Blue Crayola")!
+    static let deepSaffron = UIColor(named: "Deep Saffron")!
+    static let darkPurple = UIColor(named: "Dark Purple")!
+    static let babyPowder = UIColor(named: "Baby Powder")!
+    static let lightCyan = UIColor(named: "Light Cyan")!
+    static let pink = UIColor(named: "Pink")!
+    static let honeydew = UIColor(named: "Honeydew")!
+    static let celadon = UIColor(named: "Celadon")!
+    static let rickBlack = UIColor(named: "Rick Black FOGRA 29")!
+}
+
+
+extension UIImage {
+    static var circleBack: UIImage {
+        UIImage(named: "circle.back")!
+    }
+    
+    static var circleGear: UIImage {
+        UIImage(named: "circle.gear")!
+    }
+    
+    static var circlePen: UIImage {
+        UIImage(named: "circle.pen")!
+    }
+    
+    static var circlePlus: UIImage {
+        UIImage(named: "circle.plus")!
+    }
+    
+    static var circleTrash: UIImage {
+        UIImage(named: "circle.trash")!
+    }
+    
+}
